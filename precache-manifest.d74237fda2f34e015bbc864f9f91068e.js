06edf6b06ca20ff4b9225a02bbecd3815a689e0c
Chris Rico <chris.r.rico@gmail.com>
2020-06-27 23:34:38 -0700

    Updates

---
+++ precache-manifest.d74237fda2f34e015bbc864f9f91068e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e56aeb9296aef1a53789e8a21146fa",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "4d50ec69090034e82bd6",
+    "url": "/ReactPortfolio/static/css/main.a412264e.chunk.css"
+  },
+  {
+    "revision": "56948a9aa1f5345ee59b",
+    "url": "/ReactPortfolio/static/js/2.e46ad788.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.e46ad788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d50ec69090034e82bd6",
+    "url": "/ReactPortfolio/static/js/main.093f6e0f.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
